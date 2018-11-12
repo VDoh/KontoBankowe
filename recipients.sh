a031f7877eb3261ebca853fb41ee6bcd0dfb075d
Mateusz Chochol <mch.priv@gmail.com>
2018-11-12 19:44:51 +0100

    Improved validation in recipients.sh

--- recipients.sh
+++ recipients.sh
@@ -8,31 +8,52 @@ function cAddRecipient
     local name
     read -p "Type in recipients name: " name
     local nameState=$(cValidateWord $name)
+    if [ $nameState == 0 ]; 
+    then 
+        echo "Wrong name format. Has to start with upperscase letter, has to have at least 3 letters and only letters" 
+        sleep 3 
+        cAddRecipient 
+        return 
+    fi
 
     local surname
     read -p "Type in recipients surname: " surname
     local surnameState=$(cValidateWord $surname)
+    if [ $surnameState == 0 ]; 
+    then 
+        echo "Wrong surname format. Has to start with upperscase letter, has to have at least 3 letters and only letters" 
+        sleep 3 
+        cAddRecipient 
+        return 
+    fi
 
     local pesel
     read -p "Type in recipients pesel: " pesel
     local peselState=$(cValidateNumber $pesel 11)
+    if [ $peselState == 0 ]; 
+    then 
+        echo "Wrong pesel format. Has to have 11 digits and only digits." 
+        sleep 3 
+        cAddRecipient 
+        return 
+    fi
 
     local bankAccountNumber
     read -p "Type in recipients bank account number: " bankAccountNumber
     local bankAccountNumberState=$(cValidateNumber $bankAccountNumber 26)
-
-    if [ "$nameState" == 0 ] || [ "$surnameState" == 0 ] || [ "$peselState" == 0 ] || [ "$bankAccountNumberState" == 0 ]
-    then
-        echo "Wrong data format. Please try again..."
-        sleep 3
-        cAddRecipient
-    else
-        printf "%s" "$name " >> recipients.txt
-        printf "%s" "$surname " >> recipients.txt
-        printf "%s" "$pesel " >> recipients.txt
-        printf "%s" "$bankAccountNumber" >> recipients.txt
-        echo "" >> recipients.txt
+    if [ $bankAccountNumberState == 0 ]; 
+    then 
+        echo "Wrong bank account number format. Has to have 26 digits and only digits." 
+        sleep 3 
+        cAddRecipient 
+        return 
     fi
+
+    printf "%s" "$name " >> recipients.txt
+    printf "%s" "$surname " >> recipients.txt
+    printf "%s" "$pesel " >> recipients.txt
+    printf "%s" "$bankAccountNumber" >> recipients.txt
+    echo "" >> recipients.txt
 }
 
 function cDeleteRecipient
@@ -42,7 +63,7 @@ function cDeleteRecipient
     read -p "Type in recipients pesel: " pesel
     local peselState=$(cValidateNumber $pesel 11)
 
-    if [ "$peselState" == 0 ]
+    if [ $peselState == 0 ]
     then
         echo "Incorrect pesel. Please try again..."
         sleep 3
@@ -89,3 +110,5 @@ function cGetRecipients
         echo ""
     done
 }
+
+cGetRecipients
