e4da53afd3dd928c629c9aa4ab252b75bb2037f7
Mateusz Chochol <mch.priv@gmail.com>
2018-11-12 22:48:01 +0100

    Added creation of the savings account functionality and started creating new ones.

--- savingsGoals.sh
+++ savingsGoals.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source $(dirname $0)/usefulFunctions.sh
+
 function cCheckIfSavingsDirExists
 {
     if [ -d "$(dirname $0)/SavingsAccount" ]
@@ -24,21 +26,82 @@ function cCreateSavingsAccount
 {
     local savingsAccountDirState=$(cCheckIfSavingsDirExists)
 
-    if [ $savingsAccountDirState == 1 ]
+    if [ $savingsAccountDirState == 0 ]
     then
-        cd $(dirname $0)/SavingsAccount
-        local savingsAccountState=$(cCheckIfSavingsAccountExists)
-
-        if [ $savingsAccountState == 1]
-        then
-            echo "You already have savings account."
-            sleep 2
-            return
-        else
-            touch savingsAccount.txt
-        fi
-    else
         mkdir $(dirname $0)/SavingsAccount
+    fi
+    
+    cd $(dirname $0)/SavingsAccount
+    local savingsAccountState=$(cCheckIfSavingsAccountExists)
+
+    if [ $savingsAccountState == 0]
+    then
         touch savingsAccount.txt
     fi
+}
+
+function cSetMonthlySavings
+{
+    clear
+    local monthlySavings
+    read -p "Set how much money would you like to save per month: " monthlySavings
+    local monthlySavingsFormat='^[1-9][0-9]*$'
+
+    if ! [[ "$monthlySavings" =~ $monthlySavingsFormat ]]
+    then
+        echo "Wrong savings format. Has to be greater than 0 and can contain only digits."
+        sleep 2
+        cSetMonthlySavings
+        return
+    fi
+}
+
+function cDisplaySavingsAccountMenu
+{
+    clear
+    echo "Menu | Savings account"
+    echo "1. Set monthly savings."
+    echo "2. Make transfer manually."
+    echo "3. Display information about your savings account."
+    echo "Press desired option number in order to continue "
+}
+
+function cMakeTransfer
+{
+    clear
+
+}
+
+function cDisplaySavingsAccountInformation
+{
+    clear
+}
+
+function cSavingsAccount
+{
+    clear
+    cCreateSavingsAccount
+
+    local option
+    cDisplaySavingsAccountMenu
+    read -rsn1 option
+    local optionFormat='^[1-3]$'
+
+    if ! [[ "$option" =~ $optionFormat ]]
+    then
+        cSavingsAccount
+        return
+    fi
+
+    case $option in
+        1)
+            cSetMonthlySavings
+            ;;
+        2)
+            cMakeTransfer
+            ;;
+        3)
+            cSavingsAccount
+            ;;
+    esac
 }
