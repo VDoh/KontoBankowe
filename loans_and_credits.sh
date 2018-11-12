78860d5a8eff467e59e44e1b322302f17ee2db1a
Maciek <maciek.brylaa@gmail.com>
2018-11-12 14:47:16 +0100

    Patrz komentarze

---
+++ loans_and_credits.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function ShowInfoCreditAndLoans()
+{
+    local txtName="creditInfo.txt"     
+
+    if [ -s $txtName ]
+    then 
+        if [ `whoami`=$USER ]
+        then
+            clear
+            cat creditInfo.txt
+        else 
+            echo "You must be root to do that"
+        fi
+    else
+        echo "cannot read There might be no credits added or file doesn't exists"
+    fi
+}
+
+#zalezne od funkcji loans
+#nazwa pliku przechowujaca kredyty 
