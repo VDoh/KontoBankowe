dd07bf82015637b0e2f416129388879b45944aad
Mateusz Chochol <mch.priv@gmail.com>
2018-11-13 06:15:33 +0100

    expressTransfer.sh

---
+++ expressTransfer.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#Assumes that "balance" is the global variable for the account balance
+
+source $(dirname $0)/transfersFunctions.sh
+source $(dirname $0)/savingsAccount.sh
+
+function cExpressTransfer
+{
+    clear
+    local name=$(cGetName)
+    if [ "$name" == "-1" ]; then cExpressTransfer; return; fi
+    local surname=$(cGetSurname)
+    if [ "$surname" == "-1" ]; then cExpressTransfer; return; fi
+    local bankAccountNumber=$(cGetBankAccountNumber)
+    if [ "$bankAccountNumber" == "-1" ]; then cExpressTransfer; return; fi
+    local amount=$(cGetAmount)
+    if [ "$amount" == "-1" ]; then cExpressTransfer; return; fi
+    
+    cGenerateCode
+    cAuthentication
+
+    let balance-=$((amount+10))
+    cMakeAutomaticTransfer 5
+
+    if [ $amount -gt 49 ]
+    then
+        cValidateTransfer $amount $name $surname
+        if [ $? == 0 ]
+        then
+            clear
+            let balance+=amount
+            echo "Transfer has been reverted."
+            sleep 3
+            return
+        fi
+    fi
+
+    clear
+    echo "Your account balance is now:" $balance
+    sleep 3
+    return
+}
