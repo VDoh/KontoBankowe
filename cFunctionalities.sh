3e3c4c4425ec0de6d51b24a550e6d6284d78dbcf
Mateusz Chochol <mch.priv@gmail.com>
2018-11-14 13:35:30 +0100

    Test commit from windows laptop

--- cFunctionalities.sh
+++ cFunctionalities.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #All big functionalities that I've created in one place so it's easier to work with them as a whole: not with only parts of them.
-#There are few smaller ones that you can probably find in usefulFunctions.sh, zus.sh or any other files that you can find on my github branch.
+#There are few smaller ones that you can probably find in usefulFunctions.sh, zus.sh or any other files that you can find on my github branch
 
 source $(dirname $0)/savingsAccount.sh
 source $(dirname $0)/recipients.sh
